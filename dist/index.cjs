315754149f288bde2ff31364cf2ef58a06cc430d
Mario Meyer <mariomeyer@gmail.com>
2024-02-01 17:08:33 -0500

    module on package

--- dist/index.cjs
+++ dist/index.cjs
@@ -1,5 +1,28 @@
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
 // src/index.ts
-import { createClient, createSecureClient } from "xmlrpc";
+var src_exports = {};
+__export(src_exports, {
+  default: () => src_default
+});
+module.exports = __toCommonJS(src_exports);
+var import_xmlrpc = require("xmlrpc");
 var Odoo = class {
   config;
   host;
@@ -24,7 +47,7 @@ var Odoo = class {
     this.uid = 0;
   }
   getClient(path) {
-    const createClientFn = this.secure ? createSecureClient : createClient;
+    const createClientFn = this.secure ? import_xmlrpc.createSecureClient : import_xmlrpc.createClient;
     return createClientFn({
       host: this.host,
       port: this.port,
@@ -246,7 +269,4 @@ var Odoo = class {
   }
 };
 var src_default = Odoo;
-export {
-  src_default as default
-};
-//# sourceMappingURL=index.js.map
+//# sourceMappingURL=index.cjs.map
